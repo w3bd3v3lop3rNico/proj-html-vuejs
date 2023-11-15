0d7e816f516f3e4a4b8aba564521f94fc1ae2168
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-15 12:36:58 +0100

    Installed dontawsome Add componets and formatted App.vue Imported fontawsome to main.js Create partial scss color palette Add basic skeleton to components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import {  } from '@fortawesome/vue-fontawesome';
+
+import { faHeart as fasFaHeart, faBagShopping, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+
+import {  } from '@fortawesome/free-regular-svg-icons';
+
+import {  } from '@fortawesome/free-brands-svg-icons';
+
+library.add();
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
